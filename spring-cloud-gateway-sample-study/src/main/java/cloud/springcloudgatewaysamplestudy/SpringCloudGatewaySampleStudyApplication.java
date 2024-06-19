1ac8b8f9171a7d517bdee3db7143b6be1d6e558f
Joong Seok Hwang <hjs7949@naver.com>
2024-06-19 18:07:15 +0900

    🎉init : Init project

---
+++ spring-cloud-gateway-sample-study/src/main/java/cloud/springcloudgatewaysamplestudy/SpringCloudGatewaySampleStudyApplication.java
@@ -0,0 +1,13 @@
+package cloud.springcloudgatewaysamplestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewaySampleStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewaySampleStudyApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-gateway-sample-study/src/test/java/cloud/springcloudgatewaysamplestudy/SpringCloudGatewaySampleStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.springcloudgatewaysamplestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewaySampleStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
