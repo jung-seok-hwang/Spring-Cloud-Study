16f5da230693d98ab46330a76922034291cefdd5
Joong Seok Hwang <hjs7949@naver.com>
2024-06-20 14:50:50 +0900

    ✨ feat: Add custom filter to Spring Cloud Gateway

--- spring-cloud-gateway-sample-study/src/main/java/cloud/springcloudgatewaysamplestudy/config/FilterConfig.java
+++ spring-cloud-gateway-sample-study/src/main/java/cloud/springcloudgatewaysamplestudy/config/FilterConfig.java
@@ -6,10 +6,13 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 
 @Configuration
 public class FilterConfig {
+
+    private static final String URL = "http://localhost:8081";
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
@@ -24,6 +27,14 @@ public class FilterConfig {
                         .filters(f -> f.addRequestHeader("second-request","second-request-header")
                                 .addResponseHeader("second-response","second-response-header"))
                         .uri("http://localhost:8082"))
+                .route("rewrite_request_upper", r -> r
+                        .host("*.rewriterequestupper.org")
+                        .and()
+                        .path("/first-service/**")
+                        .filters(f -> f.prefixPath("/message")
+                                .addResponseHeader("X-TestHeader", "rewrite_request_upper")
+                                .modifyRequestBody(String.class, String.class, (exchange, s) -> Mono.just(s.toUpperCase() + s.toUpperCase()))
+                        ).uri(URL))
 
 
                 .build();
