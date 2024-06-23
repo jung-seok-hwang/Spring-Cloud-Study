fee47efe4e35945d21ed0c8d6a95bef0aa18420f
Joong Seok Hwang <hjs7949@naver.com>
2024-06-23 18:52:52 +0900

    ♻️ refactor : Updated extended gateway routing configuration and removed unnecessary features

--- spring-cloud-gateway-sample-study/src/main/java/cloud/springcloudgatewaysamplestudy/config/FilterConfig.java
+++ spring-cloud-gateway-sample-study/src/main/java/cloud/springcloudgatewaysamplestudy/config/FilterConfig.java
@@ -6,7 +6,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 
 @Configuration
@@ -27,16 +31,23 @@ public class FilterConfig {
                         .filters(f -> f.addRequestHeader("second-request","second-request-header")
                                 .addResponseHeader("second-response","second-response-header"))
                         .uri("http://localhost:8082"))
-                .route("rewrite_request_upper", r -> r
-                        .host("*.rewriterequestupper.org")
-                        .and()
-                        .path("/first-service/**")
-                        .filters(f -> f.prefixPath("/message")
-                                .addResponseHeader("X-TestHeader", "rewrite_request_upper")
-                                .modifyRequestBody(String.class, String.class, (exchange, s) -> Mono.just(s.toUpperCase() + s.toUpperCase()))
+                .route("rewrite_request_obj", r -> r.host("*.rewriterequestobj.org")
+                        .filters(f -> f.prefixPath("/first-service/message")
+                                .addResponseHeader("X-TestHeader", "read_body_pred")
                         ).uri(URL))
-
-
+                .route("rewrite_request_upper", r -> r.host("*.rewriterequestupper.org")
+                        .filters(f -> f.prefixPath("/httpbin")
+                                .addResponseHeader("X-TestHeader", "rewrite_request_upper")
+                                .modifyResponseBody(Map.class, String.class, (exchange, s) -> {
+                                    if (s == null) {
+                                        return Mono.error(new IllegalArgumentException("this should not happen"));
+                                    }
+                                    Object message = s.get("message");
+                                    Object name = s.get("name");
+                                    return Mono.just(message.toString().toUpperCase() + message.toString().toUpperCase());
+                                })
+                                .setResponseHeader("Content-Type", MediaType.TEXT_PLAIN_VALUE)
+                        ).uri(URL))
                 .build();
     }
 
