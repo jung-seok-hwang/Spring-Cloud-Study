30fe506d730d1457a3d34a562ecb28c6857b8874
Joong Seok Hwang <hjs7949@naver.com>
2024-06-24 16:18:15 +0900

    🎉init : Init project

---
+++ spring-cloud-study-user-service/src/main/java/cloud/springcloudstudyuserservice/SpringCloudStudyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.springcloudstudyuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStudyUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStudyUserServiceApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-study-user-service/src/test/java/cloud/springcloudstudyuserservice/SpringCloudStudyUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.springcloudstudyuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
