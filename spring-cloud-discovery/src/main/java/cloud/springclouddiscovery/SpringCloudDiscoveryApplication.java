79c27dd15ad91311d210d2e7734df3ebce0db38e
Joong Seok Hwang <hjs7949@naver.com>
2024-07-01 14:55:32 +0900

    🎉init : Init project

---
+++ spring-cloud-discovery/src/main/java/cloud/springclouddiscovery/SpringCloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package cloud.springclouddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDiscoveryApplication.class, args);
+    }
+
+}
